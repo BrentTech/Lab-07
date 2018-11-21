cee717f787ad1bbb763e513f42d81253b05b22c5
Brent Woodward <Brent.Woodward.1@gmail.com>
2018-11-21 13:01:49 -0800

    yelp api working

--- server.js
+++ server.js
@@ -24,6 +24,8 @@ app.get('/location', (request, response) => {
 
 app.get('/weather', getWeather);
 
+app.get('/yelp', getYelp);
+
 //Error Handling
 function handleError(err, res) {
 	console.error(err);
@@ -55,6 +57,21 @@ function getWeather(request, response) {
 		.catch(error => handleError(error, response));
 }
 
+function getYelp(request, response) {
+	const url = `https://api.yelp.com/v3/businesses/search?term=restaurants&latitude=${request.query.data.latitude}&longitude=${request.query.data.longitude}`;
+
+	superagent.get(url)
+	.set('Authorization', `Bearer ${process.env.YELP_API_KEY}`)
+	.then( result => {
+		// console.log(result);
+		const yelpBusinesses = result.body.businesses.map(restaurant => {
+			return new Yelp(restaurant);
+		});
+	response.send(yelpBusinesses);
+	})
+	.catch(error => handleError(error, response));
+}
+
 function handleError(error, res){
 	console.error(error);
 	if (res) res.status(500).send('Sorry, something broke');
@@ -72,6 +89,14 @@ function Location(query, res) {
 function Weather(day) {
 	this.forecast = day.summary;
 	this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
+
+function Yelp(restaurant) {
+	this.url = restaurant.url;
+	this.name = restaurant.name;
+	this.rating = restaurant.rating;
+	this.price = restaurant.price;
+	this.img_url = restaurant.img_url;
 }
 
 //make sure the server is listening for requests.
